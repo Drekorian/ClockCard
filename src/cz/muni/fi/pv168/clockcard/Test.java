285065c32368de2e2e4d47652a1bf1e688ade79b
David - Fires - Stein <fires@email.cz>
2011-04-05 04:20:46 +0200

    Tables add

---
+++ src/cz/muni/fi/pv168/clockcard/Test.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cz.muni.fi.pv168.clockcard;
+
+import java.sql.SQLException;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ *
+ * @author Fires
+ */
+public class Test {
+    public static void main(String[] args) throws SQLException{
+        List<Worker> list = Worker.all();
+        Iterator<Worker> iterator = list.iterator();
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+
+        Worker work = new Worker("saveTest", "saveTest", "saveTestLogin");
+            work.save();
+            work.setId(3);
+            work.destroy();
+
+    }
+}
